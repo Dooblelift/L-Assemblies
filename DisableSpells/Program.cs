bc539a1a09f398df0488f2eaf3c364d5b91e72e6
Dooblelift <OmgItzChase123@gmail.com>
2014-10-21 19:23:40 -0500

    Update Program.cs

--- DisableSpells/Program.cs
+++ DisableSpells/Program.cs
@@ -70,7 +70,7 @@ namespace DisableSpells
             Spells.Add(new SpellStruct
             {
                 ChampionName = "Syndra",
-                AvailableSpell = SpellSlot.R
+                AvailableSpell = SpellSlot.E
             });
             
             
